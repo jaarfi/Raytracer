a033751468bfdb25c9856113ed670eef89c5e879
jaarfi <day@arendt.cc>
2020-11-09 14:58:41 +0100

    display color based on angle between normalvector of hit point and light source

--- Raytracer.py
+++ Raytracer.py
@@ -4,6 +4,7 @@ import scipy
 import numpy as np
 import typing
 import time
+import math
 
 
 
@@ -26,6 +27,10 @@ class VectorFunctions(object):
     @staticmethod
     def add(vector1: Vector, vector2: Vector):
         return Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z)
+
+    @staticmethod
+    def length(vector1: Vector):
+        return np.sqrt(np.power(vector1.x,2) + np.power(vector1.y,2) + np.power(vector1.z,2))
 
     @staticmethod
     def addScalarToAll(vector1: Vector, scalar: float):
@@ -52,8 +57,12 @@ class VectorFunctions(object):
         return vector1.x*vector2.x + vector1.y*vector2.y + vector1.z*vector2.z
 
     @staticmethod
-    def normalize(vector1: Vector):           #SkalarProdukt
-        length = np.sqrt(np.power(vector1.x,2) + np.power(vector1.y,2) + np.power(vector1.z,2))
+    def angle(vector1: Vector, vector2: Vector):
+        return VectorFunctions.dotProduct(vector1, vector2) / (VectorFunctions.length(vector1) * VectorFunctions.length(vector2))
+
+    @staticmethod
+    def normalize(vector1: Vector):
+        length = VectorFunctions.length(vector1)
         return Vector(vector1.x/length,vector1.y/length, vector1.z/length)
 
 class Color(typing.NamedTuple):
@@ -61,10 +70,30 @@ class Color(typing.NamedTuple):
     g: int = 0
     b: int = 0
 
-class Sphere(typing.NamedTuple):
-    r: float
-    center: Point
-    color: Color
+class HittableObject(typing.NamedTuple):
+    def hitInPoints(self):
+        pass
+
+class Sphere(HittableObject):
+
+    def __new__(cls, r: float, center: Point, color: Color):
+        self = super(Sphere, cls).__new__(cls)
+        self.r = r
+        self.center = center
+        self.color = color
+        return self
+
+    def hitInPoints(self, point: Point, vector: Vector):
+        vectorToSphereCenter = VectorFunctions.fromTo(point, self.center)
+
+        dotProd1 = VectorFunctions.dotProduct(vector, vector)
+        dotProd2 = VectorFunctions.dotProduct(vector, vectorToSphereCenter)
+        dotProd3 = VectorFunctions.dotProduct(vectorToSphereCenter, vectorToSphereCenter)
+
+        poly = np.poly1d([dotProd1, 2 * dotProd2, dotProd3 - sphere.r * sphere.r])
+        results = poly.r
+
+        return results
 
 class ViewingScreen(typing.NamedTuple):
     bottom_left: tuple
@@ -77,51 +106,53 @@ class Camera(typing.NamedTuple):
     center: Point
     viewingScreen: ViewingScreen
 
+class LightSource(typing.NamedTuple):
+    center: Point
+    strength: float
+
 class Pixel(typing.NamedTuple):
     point: Point
 
-    def color(self, camera: Camera, sphere: Sphere):
-        vectorFromCamera = VectorFunctions.fromTo(camera.center,self.point)
-        vectorToSphereCenter = VectorFunctions.fromTo(self.point, sphere.center)
+    def color(self, camera: Camera, sphere: Sphere, light: LightSource):
+        vectorFromCamera = VectorFunctions.fromTo(camera.center, self.point)
+        results = sphere.hitInPoints(self.point, vectorFromCamera)
 
-        dotProd1 = VectorFunctions.dotProduct(vectorFromCamera, vectorFromCamera)
-        dotProd2 = VectorFunctions.dotProduct(vectorFromCamera, vectorToSphereCenter)
-        dotProd3 = VectorFunctions.dotProduct(vectorToSphereCenter, vectorToSphereCenter)
+        if (not np.iscomplex(results[0])): #Wenn er im Ersten Punkt schneidet reichts
+            intersectingPoint = Point(*VectorFunctions.add(VectorFunctions.multiply(vectorFromCamera,results[0]),camera.center))
+            normalVector = VectorFunctions.fromTo(sphere.center, intersectingPoint)
+            normalizedNormalVector = VectorFunctions.normalize(normalVector)
 
-        poly = np.poly1d([dotProd1, 2 * dotProd2, dotProd3 - sphere.r * sphere.r])
-        result = poly.r[0]
+            vectorToLight = VectorFunctions.fromTo(self.point, light.center)
+            lightIntensity = math.pow(light.strength,VectorFunctions.length(vectorFromCamera))
+            pixelColorIntensity = VectorFunctions.angle(vectorToLight, normalizedNormalVector) * lightIntensity
 
-        if (not np.iscomplex(result)): #Wenn er im Ersten Punkt schneidet reichts
-            intersecting_point = Point(*VectorFunctions.add(VectorFunctions.multiply(vectorFromCamera,result),camera.center))
-            normal_vector = VectorFunctions.fromTo(sphere.center, intersecting_point)
-            normalized_normal_vector = VectorFunctions.normalize(normal_vector)
-            color_vector = VectorFunctions.divide(normalized_normal_vector, 2)
-            color_vector = VectorFunctions.addScalarToAll(color_vector, 0.5)
-            pixel_color = Color(int(color_vector.x * 255), int(color_vector.y * 255), int(color_vector.z * 255))
+            colorAsVector = VectorFunctions.multiply(Vector(*sphere.color), abs(pixelColorIntensity))
+            pixelColor = Color(int(colorAsVector.x), int(colorAsVector.y), int(colorAsVector.z))
         else:
-            pixel_color = Color(0, 123, 255)  # Normale Farbe
+            pixelColor = Color(240,255,255)  # Normale Farbe
 
-        return pixel_color
+        return pixelColor
 
 class Main():
-    def __init__(self, sphere: Sphere, camera: Camera):
+    def __init__(self, sphere: Sphere, camera: Camera, light: LightSource):
         self.screen = camera.viewingScreen
-        self.window_width = self.screen.resolution_x
-        self.window_height = self.screen.resolution_y
-        self.picture = Picture(self.window_width, self.window_height)
+        self.windowWidth = self.screen.resolution_x
+        self.windowHeight = self.screen.resolution_y
+        self.picture = Picture(self.windowWidth, self.windowHeight)
+        self.light = light
 
         self.calculatePicture()
         self.drawPicture()
 
     def calculatePicture(self):
         now = time.time()
-        for i in range(1, self.window_width):
-            for j in range(1, self.window_height):
-                currentPixelPoint = Point(*VectorFunctions.add(self.screen.bottom_left,Vector(self.screen.width/self.window_width*i,self.screen.height/self.window_height*j,0)))
+        for i in range(1, self.windowWidth):
+            for j in range(1, self.windowHeight):
+                currentPixelPoint = Point(*VectorFunctions.add(self.screen.bottom_left, Vector(self.screen.width / self.windowWidth * i, self.screen.height / self.windowHeight * j, 0)))
                 currentPixel = Pixel(currentPixelPoint)
 
 
-                self.picture.insertPixel(currentPixel.color(camera,sphere), i, j)
+                self.picture.insertPixel(currentPixel.color(camera,sphere,light), i, j)
 
         print(time.time()-now)
 
@@ -140,7 +171,11 @@ class Picture():
     def insertPixel(self, color: Color, x, y):
         self.values[x][y] = color
 
-camera = Camera(Point(0,0,0),ViewingScreen(Point(-2,-1,-4),4,4,200,200))
-sphere = Sphere(0.4, Point(0,0,-2), Color(0,255,0))
-main = Main(sphere, camera)
+camera = Camera(Point(0,0,0),ViewingScreen(Point(-2,-1,-4),4,4,256,256))
+hit = HittableObject()
+point = Point(0,0,-2)
+color = Color(255,0,255)
+sphere = Sphere(0.5, point, color)
+light = LightSource(Point(-2, 2, 0),0.99)
+main = Main(sphere, camera, light)
 v = Vector(0,0,0)
