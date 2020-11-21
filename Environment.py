4ddeb5117d2be2a26acd3f32a6bb51040e0122be
Gidde <gideon.gratis@gmail.com>
2020-11-21 15:15:59 +0100

    Wall 1 steht

--- Environment.py
+++ Environment.py
@@ -18,16 +18,25 @@ class Environment:
 
         self.buildEnvironmentOne()
 
+
     def buildEnvironmentOne(self):
         self.env.append(pg.Polygon(0,1,1, 0,0,1, 0,0,0))
         self.env.append(pg.Polygon(0,0,1, 1,0,1, 0,0,0))
+
+        # Wall 1
+        self.env.append(pg.Polygon(0,5,0, 5,5,5, 0,0,5))
+        self.env.append(pg.Polygon(5,5,0, 5,5,5, 0,5,5))
+
         self.plotEnvironment()
+
 
     def plotEnvironment(self):
         fig = plt.figure()
         ax = Axes3D(fig)
         ax.add_collection3d(Poly3DCollection(self.env[0].getVert()), self.env[0].getZS(), zdir='y')
         ax.add_collection3d(Poly3DCollection(self.env[1].getVert()), self.env[1].getZS(), zdir='y')
+        ax.add_collection3d(Poly3DCollection(self.env[2].getVert()), self.env[2].getZS(), zdir='y')
+        ax.add_collection3d(Poly3DCollection(self.env[3].getVert()), self.env[3].getZS(), zdir='y')
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
