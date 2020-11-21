be7b8c1c2b33c699085c59ac872eb061ded36b6a
Gidde <gideon.gratis@gmail.com>
2020-11-21 15:28:13 +0100

    Wall 2 steht und draw über schleife

--- Environment.py
+++ Environment.py
@@ -20,12 +20,13 @@ class Environment:
 
 
     def buildEnvironmentOne(self):
-        self.env.append(pg.Polygon(0,1,1, 0,0,1, 0,0,0))
-        self.env.append(pg.Polygon(0,0,1, 1,0,1, 0,0,0))
-
         # Wall 1
         self.env.append(pg.Polygon(0,5,0, 5,5,5, 0,0,5))
         self.env.append(pg.Polygon(5,5,0, 5,5,5, 0,5,5))
+
+        # Wall 2
+        self.env.append(pg.Polygon(0,0,0, 0,5,5, 0,0,5))
+        self.env.append(pg.Polygon(0,0,0, 0,0,5, 0,5,5))
 
         self.plotEnvironment()
 
@@ -33,20 +34,19 @@ class Environment:
     def plotEnvironment(self):
         fig = plt.figure()
         ax = Axes3D(fig)
-        ax.add_collection3d(Poly3DCollection(self.env[0].getVert()), self.env[0].getZS(), zdir='y')
-        ax.add_collection3d(Poly3DCollection(self.env[1].getVert()), self.env[1].getZS(), zdir='y')
-        ax.add_collection3d(Poly3DCollection(self.env[2].getVert()), self.env[2].getZS(), zdir='y')
-        ax.add_collection3d(Poly3DCollection(self.env[3].getVert()), self.env[3].getZS(), zdir='y')
+
+        for i in range(len(self.env)):
+            ax.add_collection3d(Poly3DCollection(self.env[i].getVert()), self.env[i].getZS(), zdir='y')
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
 
-        ax.set_xlim3d(0, 5)
-        ax.set_ylim3d(0, 5)
-        ax.set_zlim3d(0, 5)
+        ax.set_xlim3d(0, self.Qsize)
+        ax.set_ylim3d(0, self.Qsize)
+        ax.set_zlim3d(0, self.Qsize)
 
         plt.show()
 
 
-env1 = Environment(200)
+env1 = Environment(5)
